705bd606910b95c154179474dbdfb9352ebf665c
Angel Ortega <cs.angelortega@gmail.com>
2018-01-31 12:35:24 -0800

    Add test fixtures for TreeADT

--- src/test/scala/TreeADTTest.scala
+++ src/test/scala/TreeADTTest.scala
@@ -1,3 +1,38 @@
-class TreeADTTest {
+import org.scalatest.{FlatSpec, Matchers}
+
+class TreeADTTest extends FlatSpec with Matchers {
+
+  "A Search Tree" should "insert in an ordered sequence" in {
+
+  }
+
+  "A search tree" should "print a preorder traversal" in {
+
+  }
+
+  "A search tree" should "print a postorder traversal" in {
+
+  }
+
+  "A search tree" should "print an inorder traversal" in {
+
+  }
+
+  "A search tree" should "print a BFS traversal" in {
+
+  }
+
+  "A search tree" should "print a DFS traversal" in {
+
+  }
+
+  "A search tree" should "print a path to find a node (using BFS)" in {
+
+  }
+
+  "A search tree" should "print a path to find a node (using DFS)" in {
+
+  }
+
 
 }
