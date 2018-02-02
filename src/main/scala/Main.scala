ae1618609e7639d394f94bfd60600ed6da60cd2d
Angel Ortega <cs.angelortega@gmail.com>
2018-02-02 13:06:16 -0800

    Pushing Graph class and Vertex class, these are utility classes for solving graph problems

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,31 +1,35 @@
 package chapter4
 
 object Main extends App {
-  val x = new TreeADT
-  x.insert(-10)
-  println(x)
-  println(x.isBalanced())
 
-  x.insert(2)
-  println(x)
-  println(x.isBalanced())
-  x.insert(100)
-  println(x)
-  println(x.isBalanced())
+  val x = Graph(2)
+  x.addEdge(Vertex(0), Vertex(1))
+  x.addEdge( Vertex(1), Vertex(1))
 
-
-  x.insert(-200)
-  println(x)
-  println(x.isBalanced())
-
-  x.insert(-7)
-  println(x)
-  println(x.isBalanced())
-
-  x.insert(-9)
-  println(x)
-  println(x.isBalanced())
-
+  //  val x = new TreeADT
+  //  x.insert(-10)
+  //  println(x)
+  //  println(x.isBalanced())
+  //
+  //  x.insert(2)
+  //  println(x)
+  //  println(x.isBalanced())
+  //  x.insert(100)
+  //  println(x)
+  //  println(x.isBalanced())
+  //
+  //
+  //  x.insert(-200)
+  //  println(x)
+  //  println(x.isBalanced())
+  //
+  //  x.insert(-7)
+  //  println(x)
+  //  println(x.isBalanced())
+  //
+  //  x.insert(-9)
+  //  println(x)
+  //  println(x.isBalanced())
 
   //  x.insert(-1000)
 //  x.insert(-1110)
