4377fb1f7708493782c3a80d076a93a081ce9999
Angel Ortega <cs.angelortega@gmail.com>
2018-02-14 23:29:21 -0800

    Begin chapter 10 sorting algorithms

---
+++ src/main/scala/chapter10/SelectionSort.scala
@@ -0,0 +1,26 @@
+package chapter10
+
+object SelectionSort {
+
+  /** Selection sort operates by finding the minimum or maximum, 
+    * and swapping to the front
+    * 
+    * Time Complexity: O(n^2)
+    * @param x is an unordered sequence
+    */
+  def sort[A](unordered: Seq[A])(implicit ordered: Ordered[A]): Seq[A] = {
+    unordered match {
+      case Nil => Nil
+      case x :: Nil => x
+      case x :: xs => {
+        val minRight = xs.reduce(_ min _)
+        if (minRight < x)
+          minRight :: SelectionSort.sort(x :: (xs -- minRight))
+        else
+          x :: SelectionSort.sort(minRight :: xs)
+      }
+    }
+  }
+
+
+}
