dedbd711afeb2dad255dba887f172f0ccd41afd8
Angel Ortega <cs.angelortega@gmail.com>
2018-03-09 19:53:14 -0800

    Added bit manipulation problem: Insertion

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,41 +1,41 @@
-package chapter4
-
-object Main extends App {
-
-  val x = Graph(2)
-  x.addEdge(Vertex(0), Vertex(1))
-  x.addEdge( Vertex(1), Vertex(1))
-
-  //  val x = new TreeADT
-  //  x.insert(-10)
-  //  println(x)
-  //  println(x.isBalanced())
-  //
-  //  x.insert(2)
-  //  println(x)
-  //  println(x.isBalanced())
-  //  x.insert(100)
-  //  println(x)
-  //  println(x.isBalanced())
-  //
-  //
-  //  x.insert(-200)
-  //  println(x)
-  //  println(x.isBalanced())
-  //
-  //  x.insert(-7)
-  //  println(x)
-  //  println(x.isBalanced())
-  //
-  //  x.insert(-9)
-  //  println(x)
-  //  println(x.isBalanced())
-
-  //  x.insert(-1000)
-//  x.insert(-1110)
-//  x.insert(-1)
-//  println(x)
-//  x.insert(1)
-//  println(x)
-
-}
+//package chapter4
+//
+//object Main extends App {
+//
+//  val x = Graph(2)
+//  x.addEdge(Vertex(0), Vertex(1))
+//  x.addEdge( Vertex(1), Vertex(1))
+//
+//  //  val x = new TreeADT
+//  //  x.insert(-10)
+//  //  println(x)
+//  //  println(x.isBalanced())
+//  //
+//  //  x.insert(2)
+//  //  println(x)
+//  //  println(x.isBalanced())
+//  //  x.insert(100)
+//  //  println(x)
+//  //  println(x.isBalanced())
+//  //
+//  //
+//  //  x.insert(-200)
+//  //  println(x)
+//  //  println(x.isBalanced())
+//  //
+//  //  x.insert(-7)
+//  //  println(x)
+//  //  println(x.isBalanced())
+//  //
+//  //  x.insert(-9)
+//  //  println(x)
+//  //  println(x.isBalanced())
+//
+//  //  x.insert(-1000)
+////  x.insert(-1110)
+////  x.insert(-1)
+////  println(x)
+////  x.insert(1)
+////  println(x)
+//
+//}
--- src/main/scala/chapter5/Insertion.scala
+++ src/main/scala/chapter5/Insertion.scala
@@ -10,28 +10,41 @@ object Insertion extends App {
     * Write a method to set all bits between i and j in N equal to M (e.g., M becomes
     * a substring of N located at i and starting at j).
     * EXAMPLE:
-    * Input: N = 10000000000, M = 10101, i = 2, j = 6
+    * Input:  N = 10000000000, M = 10101, i = 2, j = 6
     * Output: N = 10001010100
     *
     */
-  def validate(N: Int, M: Int, i: Int, j: Int) = {
+  def validate(n: Int, m: Int, i: Int, j: Int) = {
 
     // n      = 10000000000
     // m      = 00000010101
-    // output = 10001010100
 
-    // m_mask = 1111mmmmm11
-    // n_mask = 10001111100
+    val ones = ~0
+    // x      = 11111111111
 
-    // m_mask & n_mask
-    (M << i) | N
+    val left = ones << (j + 1)   // x1 = 11100000000
+
+    val right = (1 << i) - 1   // right = 00000000011
+
+    val mask = left | right
+    // other  = 11100000011
+
+    val n_mask = n & mask  // clear bits between n
+    // n_mask  = 10000000000
+
+    val m_shift = m << i
+    // m_shift = 00001010100
+
+    m_shift | n_mask
+
   }
 
-  val x = 2^10
-  val y = 2^0 + 2^2 + 2^4
+  val x = math.pow(2,10).toInt
+  val y = (math.pow(2,0) + math.pow(2,2) + math.pow(2,4)).toInt
 
-  val expected = 2^2 + 2^4 + 2^6
+  val expected = math.pow(2,2) + math.pow(2,4) + math.pow(2,6)
 
-  println(validate(x, y, 2, 6))
+  // BitMap(10001010100) == 1108
+  assert(1108 == validate(x, y, 2, 6))
 
 }
