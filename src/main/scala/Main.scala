9e99504e682352001053d95c1fba25fd2f5f72d8
Angel <cs.angelortega@gmail.com>
2018-06-17 22:22:58 -0700

    FixedStack Solution without using ClassTags to set the array

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,25 +1,23 @@
-import chapter3.book.{MyQueue, MyStack}
+import chapter3.solutions.FixedStack
 
 object Main extends App {
-  val s = new MyStack[Int]
-  s.push(1)
-  s.push(2)
-  s.push(3)
-  // Stack elements
-  //          =>               =>               =>   [  None ]
-  //          =>               =>   [  None ]   =>   [Some(3)]
-  //          =>   [  None ]   =>   [Some(2)]   =>   [Some(2)]
-  //  [None]  =>   [Some(1)]   =>   [Some(1)]   =>   [Some(1)]
-  println(s.peek())
+  val stacks = new FixedStack[Int](3)
+  stacks.push(1, 0)
+  stacks.push(2, 0)
+  stacks.push(3, 0)
 
-  val q = new MyQueue[Int]
-  q.enqueue(1)
-  q.enqueue(2)
-  q.enqueue(3)
-  // Queue elements
-  // first*                                        [None] *last
-  // first*                           [None] <- [Some(1)] *last
-  // first*              [None] <- [Some(2)] <- [Some(1)] *last
-  // first* [None] <- [Some(3)] <- [Some(2)] <- [Some(1)] *last
-  println(q.peek())
+  stacks.push(4, 1)
+  stacks.push(5, 1)
+  stacks.push(6, 1)
+
+  stacks.push(7, 2)
+  stacks.push(8, 2)
+  stacks.push(9, 2)
+
+  val a1 = stacks.pop(0)
+  val a2 = stacks.pop(0)
+  val t1 = stacks.peek(0)
+  val t2 = stacks.peek(0)
+  val t3 = stacks.peek(0)
+
 }
--- src/main/scala/chapter3/solutions/ThreeInOne.scala
+++ src/main/scala/chapter3/solutions/ThreeInOne.scala
@@ -1,39 +1,37 @@
 package chapter3.solutions
 
-class FixedStack[Option[T]](val stackCapacity: Int) {
+class FixedStack[T](val stackCapacity: Int) {
 
   val arr: Array[Option[T]] = Array.fill(stackCapacity * 3)(None)
   val sz: Array[Int] = Array.fill(3)(0)
 
   def push(item: T, stackID: Int): Unit = {
-    require(stackID > = 0 && stackID < stackCapacity)
+    require(stackID >= 0 && stackID < stackCapacity, s"StackID must be in bounds of [0 - ${stackCapacity - 1}], inclusive")
 
     if (sz(stackID) == stackCapacity)
-      throw new StackOverflowException()
+      throw new StackOverflowError()
 
     arr(stackID * 3 + sz(stackID)) = Some(item)
     sz(stackID) += 1
   }
 
   def pop(stackID: Int): T = {
-    require(stackID > = 0 && stackID < stackCapacity)
+    require(stackID >= 0 && stackID < stackCapacity)
     if (sz(stackID) == 0)
-      throw new NoSuchElementException()
+      throw new NoSuchElementException("Stack is empty")
 
+    sz(stackID) -= 1
     val item = arr(stackID * 3 + sz(stackID))
     arr(stackID * 3 + sz(stackID)) = None
-    sz(stackID) -= 1
     item.get
   }
 
   def peek(stackID: Int): T = {
     if (isEmpty(stackID))
       throw new NoSuchElementException("Stack is empty")
-    arr(3 * stackID + sz(stackID)).get
+    arr(3 * stackID + sz(stackID) - 1).get
   }
 
   def isEmpty(stackID: Int): Boolean = sz(stackID) == 0
-
-
 
 }
